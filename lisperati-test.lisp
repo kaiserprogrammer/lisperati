bbb37179ae790f4e022517dd54e92b48a1ebac32
Jürgen Bickert <juergenbickert@gmail.com>
2012-07-13 02:12:35 +0200

    make it faster

--- lisperati-test.lisp
+++ lisperati-test.lisp
@@ -7,45 +7,41 @@
 
 (test no-substitution
   (let ((templ "blub"))
-   (is (equal "blub" (insert-template (compile-template templ))))))
+    (is (equal "blub" (render-template (compile-template templ))))))
 
 (test with-subsititution
-  (is (equal "blub1" (insert-template (compile-template "blub(=1)")))))
+  (is (equal "blub1" (render-template (compile-template "blub(=1)")))))
 
 (test with-binding
-  (let* ((*counter* 2)
-        (template "blub(=*counter*)")
-         (compiled-template (compile-template template)))
-    (declare (special *counter*))
-    (is (equal "blub2" (insert-template compiled-template)))))
+  (let* ((counter 2)
+         (template "blub(=counter)"))
+    (declare (special counter))
+    (is (equal "blub2" (render-template (compile-template template))))))
 
 (test multiple-substitution
-  (let ((*counter* 1))
-    (declare (special *counter*))
+  (let ((counter 1))
+    (declare (special counter))
     (is (equal "blub1blub2blub3"
-               (insert-template
-                (compile-template
-                 "blub(=*counter*)blub(=(incf *counter*))blub(=(incf *counter*))"))))))
+               (render-template (compile-template
+                "blub(=counter)blub(=(incf counter))blub(=(incf counter))"))))))
 
 (test lisp-mix
   (is (equal
        "<div class=\"1\"></div><div class=\"2\"></div><div class=\"3\"></div>"
-       (insert-template
-        (compile-template
-         "((loop for i in (list 1 2 3)
+       (render-template (compile-template
+        "((loop for i in (list 1 2 3)
               do (with-template <div class=\"(=i)\"></div>)))")))))
 
 (test file-templates
-  (let ((compiled-template (compile-file-template (make-pathname
-                                                   :name "test.lr"
-                                                   :directory
-                                                   (pathname-directory
-                                                    (pathname
-                                                     (or *compile-file-truename*
-                                                         *load-truename*))))))
-        (*counter* 1))
+  (let ((*counter* 1))
     (declare (special *counter*))
-    (is (equal "blub1" (insert-template compiled-template)))))
+    (is (equal "blub1"
+               (render-template (compile-file-template (relative-file "test.lr"))))))
+  (let ((*counter* 2)
+        (file (relative-file "test.lr")))
+    (declare (special *counter*))
+    (is (equal "blub2"
+               (render-template (compile-file-template file))))))
 
 (test relative-file
   (is (equal "true" (get-whole-file-as-string (relative-file "relative.test"))))
--- lisperati.lisp
+++ lisperati.lisp
@@ -2,17 +2,19 @@
   (:use :cl :cl-ppcre)
   (:export
    :compile-template
-   :insert-template
+   :render-template
    :compile-file-template
    :relative-file
    :get-whole-file-as-string))
 (in-package :lisperati)
 
 (defun compile-file-template (file)
-  (compile-template (get-whole-file-as-string (pathname file))))
+  (let ((content (get-whole-file-as-string (pathname file))))
+    (compile-template content)))
 
 (defun compile-template (template)
-  `(concatenate 'string ,@(find-snippets template)))
+  (let ((snippets (find-snippets template)))
+    (compile nil `(lambda () (concatenate 'string ,@snippets)))))
 
 (defun find-snippets (template)
   (apply
@@ -23,39 +25,39 @@
            (if (string= string-found "(=")
                (multiple-value-bind (exp length)
                    (read-from-string (subseq template (+ found 2)))
-                 (print`((,(subseq template 0 found)
-                      (princ-to-string ,exp))
-                    ,(find-snippets (subseq template (+ found length 3))))))
+                 `((,(subseq template 0 found)
+                     (princ-to-string ,exp))
+                   ,(find-snippets (subseq template (+ found length 3)))))
                (let ((template-found (scan "\\(with-template" template :start found)))
                  (if template-found
                      (multiple-value-bind (exp inner-length)
                          (read-from-string (subseq template template-found))
+                       (declare (ignore exp))
                        (let ((compiled-template
                               (concatenate
                                'string
                                "(with-output-to-string (s) "
                                (subseq template (1+ found) template-found)
-                               (format nil "(format s ~w)" (compile-template (subseq template (+ 15 template-found) (1- (+ template-found inner-length)))))
+                               (let ((blub (subseq template (+ 15 template-found) (1- (+ template-found inner-length)))))
+                                 (format nil "(format s \"~~{~~a~~}\" ~w)"
+                                         (cons 'list (find-snippets blub))))
                                (subseq template (+ template-found inner-length)))))
-                         (print compiled-template)
-                         (print template)
-                         (print (+ found inner-length))
                          (multiple-value-bind (exp length)
                              (read-from-string compiled-template)
-                           (print `((,(subseq template 0 found)
-                                     ,exp
-                                     ,@(find-snippets (subseq template (+ template-found inner-length 2)))))))))
+                           (declare (ignore length))
+                           `((,(subseq template 0 found)
+                               ,exp
+                               ,@(find-snippets (subseq template (+ template-found inner-length 2))))))))
                      (multiple-value-bind (exp length)
                          (read-from-string (subseq template found))
-                       (progn (print exp)
-                        `((,(subseq template 0 found)
-                            (with-output-to-string (s)
-                              ,exp)
-                            ,(find-snippets (subseq template (+ found length 2)))))))))))
+                       `((,(subseq template 0 found)
+                           (with-output-to-string (s)
+                             ,exp)
+                           ,(find-snippets (subseq template (+ found length 2))))))))))
          (list (list template))))))
 
-(defun insert-template (compiled-template)
-  (eval compiled-template))
+(defun render-template (compiled-template)
+  (funcall compiled-template))
 
 (defun get-whole-file-as-string (name)
   (with-open-file (in name)
@@ -66,20 +68,11 @@
           (subseq text 0 read)
           text))))
 
-(defun relative-file (name)
-  (make-pathname
-   :name name
-   :directory
-   (pathname-directory
-    (or *load-truename*
-        *compile-file-truename*))))
+(defmacro relative-file (name)
+  `(make-pathname
+    :name ,name
+    :directory
+    (pathname-directory
+     (or ,*load-truename*
+         ,*compile-file-truename*))))
 
-
-
-
-;; (with-output-to-string (s)
-;;   (loop for i in (list 1 2 3)
-;;      do (format s (concatenate 'string
-;;                                "<div class=\""
-;;                                (princ-to-string i)
-;;                                "\"></div>"))))
